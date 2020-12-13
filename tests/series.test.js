8c24dcde0ef8fffa75933d5830e789faeac98655
Sam Ireland <sam.ireland.uk@gmail.com>
2020-12-13 22:19:00 +0000

    Centralise test data

--- tests/series.test.js
+++ tests/series.test.js
@@ -1,45 +1,39 @@
 const {validateTimestampAgainstResolution} = require("../js/series");
+const testData = require("./test-data.json");
 
 test("timestamps with no resolution always validate", () => {
-  expect(validateTimestampAgainstResolution(0, null)).toBe(true);
-  expect(validateTimestampAgainstResolution(-100, null)).toBe(true);
-  expect(validateTimestampAgainstResolution(1000, null)).toBe(true);
+  for (let testCase of testData.validateTimestampAgainstResolution.noResolution) {
+    const [ts, res] = testCase.input;
+    expect(validateTimestampAgainstResolution(ts, res)).toBe(testCase.output);
+  }
 });
 
 test("timestamps with D resolution validate", () => {
-  expect(validateTimestampAgainstResolution(0, "D")).toBe(true);
-  expect(validateTimestampAgainstResolution(100, "D")).toBe(false);
-  expect(validateTimestampAgainstResolution(86400, "D")).toBe(true);
-  expect(validateTimestampAgainstResolution(-86400, "D")).toBe(true);
-  expect(validateTimestampAgainstResolution(-80000, "D")).toBe(false);
+  for (let testCase of testData.validateTimestampAgainstResolution.D) {
+    const [ts, res] = testCase.input;
+    expect(validateTimestampAgainstResolution(ts, res)).toBe(testCase.output);
+  }
 });
 
 test("timestamps with W resolution validate", () => {
-  expect(validateTimestampAgainstResolution(0, "W")).toBe(false);
-  expect(validateTimestampAgainstResolution(86400, "W")).toBe(false);
-  expect(validateTimestampAgainstResolution(946684800, "W")).toBe(false);
-  expect(validateTimestampAgainstResolution(946684801, "W")).toBe(false);
-  expect(validateTimestampAgainstResolution(1605484800, "W")).toBe(true);
-  expect(validateTimestampAgainstResolution(-259200, "W")).toBe(true);
+  for (let testCase of testData.validateTimestampAgainstResolution.W) {
+    const [ts, res] = testCase.input;
+    expect(validateTimestampAgainstResolution(ts, res)).toBe(testCase.output);
+  }
 });
 
 test("timestamps with M resolution validate", () => {
-  expect(validateTimestampAgainstResolution(1, "M")).toBe(false);
-  expect(validateTimestampAgainstResolution(86400, "M")).toBe(false);
-  expect(validateTimestampAgainstResolution(946684800, "M")).toBe(true);
-  expect(validateTimestampAgainstResolution(946684801, "M")).toBe(false);
-  expect(validateTimestampAgainstResolution(1605484800, "M")).toBe(false);
-  expect(validateTimestampAgainstResolution(1201824000, "M")).toBe(true);
+  for (let testCase of testData.validateTimestampAgainstResolution.M) {
+    const [ts, res] = testCase.input;
+    expect(validateTimestampAgainstResolution(ts, res)).toBe(testCase.output);
+  }
 });
 
 test("timestamps with Y resolution validate", () => {
-  expect(validateTimestampAgainstResolution(1, "Y")).toBe(false);
-  expect(validateTimestampAgainstResolution(86400, "Y")).toBe(false);
-  expect(validateTimestampAgainstResolution(946684800, "Y")).toBe(true);
-  expect(validateTimestampAgainstResolution(946684801, "Y")).toBe(false);
-  expect(validateTimestampAgainstResolution(1605484800, "Y")).toBe(false);
-  expect(validateTimestampAgainstResolution(1201824000, "Y")).toBe(false);
-  expect(validateTimestampAgainstResolution(1199145600, "Y")).toBe(true);
+  for (let testCase of testData.validateTimestampAgainstResolution.Y) {
+    const [ts, res] = testCase.input;
+    expect(validateTimestampAgainstResolution(ts, res)).toBe(testCase.output);
+  }
 });
 
 
--- tests/test_series.py
+++ tests/test_series.py
@@ -1,45 +1,37 @@
+import json
 from unittest import TestCase
 from python.series import *
+
+with open("tests/test-data.json") as f:
+    test_data = json.load(f)
 
 class TimestampValidationTests(TestCase):
 
     def test_timestamps_with_no_resolution(self):
-        self.assertTrue(validate_timestamp_against_resolution(0, None))
-        self.assertTrue(validate_timestamp_against_resolution(-100, None))
-        self.assertTrue(validate_timestamp_against_resolution(1000, None))
-    
+        for test_case in test_data["validateTimestampAgainstResolution"]["noResolution"]:
+            ts, res = test_case["input"]
+            self.assertEqual(validate_timestamp_against_resolution(ts, res), test_case["output"])
+
 
     def test_timestamps_with_D_resolution(self):
-        self.assertTrue(validate_timestamp_against_resolution(0, "D"))
-        self.assertFalse(validate_timestamp_against_resolution(100, "D"))
-        self.assertTrue(validate_timestamp_against_resolution(86400, "D"))
-        self.assertTrue(validate_timestamp_against_resolution(-86400, "D"))
-        self.assertFalse(validate_timestamp_against_resolution(-80000, "D"))
+        for test_case in test_data["validateTimestampAgainstResolution"]["D"]:
+            ts, res = test_case["input"]
+            self.assertEqual(validate_timestamp_against_resolution(ts, res), test_case["output"])
     
 
     def test_timestamps_with_W_resolution(self):
-        self.assertFalse(validate_timestamp_against_resolution(0, "W"))
-        self.assertFalse(validate_timestamp_against_resolution(86400, "W"))
-        self.assertFalse(validate_timestamp_against_resolution(946684800, "W"))
-        self.assertFalse(validate_timestamp_against_resolution(946684801, "W"))
-        self.assertTrue(validate_timestamp_against_resolution(1605484800, "W"))
-        self.assertTrue(validate_timestamp_against_resolution(-259200, "W"))
+        for test_case in test_data["validateTimestampAgainstResolution"]["W"]:
+            ts, res = test_case["input"]
+            self.assertEqual(validate_timestamp_against_resolution(ts, res), test_case["output"])
 
 
     def test_timestamps_with_M_resolution(self):
-        self.assertFalse(validate_timestamp_against_resolution(1, "M"))
-        self.assertFalse(validate_timestamp_against_resolution(86400, "M"))
-        self.assertTrue(validate_timestamp_against_resolution(946684800, "M"))
-        self.assertFalse(validate_timestamp_against_resolution(946684801, "M"))
-        self.assertFalse(validate_timestamp_against_resolution(1605484800, "M"))
-        self.assertTrue(validate_timestamp_against_resolution(1201824000, "M"))
+        for test_case in test_data["validateTimestampAgainstResolution"]["M"]:
+            ts, res = test_case["input"]
+            self.assertEqual(validate_timestamp_against_resolution(ts, res), test_case["output"])
     
 
     def test_timestamps_with_Y_resolution(self):
-        self.assertFalse(validate_timestamp_against_resolution(1, "Y"))
-        self.assertFalse(validate_timestamp_against_resolution(86400, "Y"))
-        self.assertTrue(validate_timestamp_against_resolution(946684800, "Y"))
-        self.assertFalse(validate_timestamp_against_resolution(946684801, "Y"))
-        self.assertFalse(validate_timestamp_against_resolution(1605484800, "Y"))
-        self.assertFalse(validate_timestamp_against_resolution(1201824000, "Y"))
-        self.assertTrue(validate_timestamp_against_resolution(1199145600, "Y"))
+        for test_case in test_data["validateTimestampAgainstResolution"]["Y"]:
+            ts, res = test_case["input"]
+            self.assertEqual(validate_timestamp_against_resolution(ts, res), test_case["output"])
